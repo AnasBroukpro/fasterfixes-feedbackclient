9c2f8f4e8137d0dd2ea3a911b9ef01069070ebe4
Manuel Coffin <manuel.coffin@gmail.com>
2026-06-28 09:58:09 +0200

    feat(web): add dismissable TinyLaunch launch banner on public layout

--- apps/web/src/app/(public)/layout.tsx
+++ apps/web/src/app/(public)/layout.tsx
@@ -1,10 +1,12 @@
 import { LayoutParams } from "@/types/next";
 import { Footer } from "../_features/core/footer/footer";
 import { Header } from "../_features/core/header/header.server";
+import { LaunchBanner } from "../_features/core/launch-banner/launch-banner.client";
 
 export default function PublicLayout({ children }: LayoutParams) {
   return (
     <div>
+      <LaunchBanner />
       <Header />
       {children}
 
